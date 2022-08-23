60bb5b5c1a9b23b4cb18e5ed568629e240614a6a
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2022-08-23 15:16:05 -0600

    Debería de funcionar

--- Taq_con_Back_EMF/Taq_con_Back_EMF.ino
+++ Taq_con_Back_EMF/Taq_con_Back_EMF.ino
@@ -78,6 +78,36 @@ void setup() {
   // y A0
 
   analogComparator.enableInterrupt(Back_emf, RISING);
+  //Ejecución en bucle abierto
+  int i = 0;
+  while (i < 20) {
+    // La corriente entra Por A y sale por B
+    digitalWrite(Cup, 0);
+    digitalWrite(Aup, 1);
+    delay(DELAY);
+    //La corriente Entre Por A y sale por C
+    digitalWrite(Bdown, 0);
+    digitalWrite(Cdown, 1);
+    delay(DELAY);
+    // La Corriente entra por B y Sale por C
+    digitalWrite(Aup, 0);
+    digitalWrite(Bup, 1);
+    delay(DELAY);
+    // La corriente entra por B y sale por A
+    digitalWrite(Cdown, 0);
+    digitalWrite(Adown, 1);
+    delay(DELAY);
+    //La corriente entra por C y sale por A
+    digitalWrite(Cup, 1);
+    digitalWrite(Bup, 0);
+    delay(DELAY);
+    // la corriente entra por C y sale por B
+    digitalWrite(Adown, 0);
+    digitalWrite(Bdown, 1);
+    delay(DELAY);
+    i++;
+    //
+  }
 
 }
 
